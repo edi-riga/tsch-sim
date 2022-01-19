dae34c0e7af43d9cce873a33772c75dea44f3843
Atis Elsts <atis.elsts@gmail.com>
2022-01-19 19:51:14 +0200

    fix launching when the configuration file name with has spaces in the path

--- tsch-sim-windows.bat
+++ tsch-sim-windows.bat
@@ -3,14 +3,14 @@ rem This file starts the simulator on Microsoft Windows
 
 if "%1"=="" goto no_args
 
-set CONFIG_FILE=%~f1
-if exist %CONFIG_FILE%\NUL set CONFIG_FILE=%CONFIG_FILE%\config.json
+set CONFIG_FILE="%~f1"
+if exist "%CONFIG_FILE%\NUL" set CONFIG_FILE="%CONFIG_FILE%\config.json"
 
 echo "CONFIG_FILE=%CONFIG_FILE%"
 set WORKDIR=%~dp0
 cd %WORKDIR%
 
-node --harmony --experimental-modules %NODE_ARGUMENTS% source/main.mjs %CONFIG_FILE%
+node --harmony --experimental-modules %NODE_ARGUMENTS% source/main.mjs "%CONFIG_FILE%"
 
 exit /b %errorlevel%
 
